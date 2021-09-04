bdf6689105c5a3294d90fca38fabc629dc1b8b10
unknown <tarik.pasalic777@gmail.com>
2021-09-04 18:59:07 +0200

    login with oAuth

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,52 @@
+import React,{useState} from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+
+const clientId = "529021270692-dkbtlhpg4ljq7r873jhrm9v24sth1eki.apps.googleusercontent.com";
+
 const Header =()=>{
 
-  
-    return (<div>
-        Header
-    </div>)
+    const [showloginButton, setShowloginButton] = useState(true);
+    const [showlogoutButton, setShowlogoutButton] = useState(false);
+    const onLoginSuccess = (res) => {
+        console.log('Login Success:', res.profileObj);
+        setShowloginButton(false);
+        setShowlogoutButton(true);
+    };
 
+    const onLoginFailure = (res) => {
+        console.log('Login Failed:', res);
+    };
+
+    const onSignoutSuccess = () => {
+        alert("You have been logged out successfully");
+        console.clear();
+        setShowloginButton(true);
+        setShowlogoutButton(false);
+    };
+
+    return (
+        <div>
+            <h1>Header</h1>
+            { showloginButton ?
+                <GoogleLogin
+                    clientId={clientId}
+                    buttonText="Sign In"
+                    onSuccess={onLoginSuccess}
+                    onFailure={onLoginFailure}
+                    cookiePolicy={'single_host_origin'}
+                    isSignedIn={true}
+                /> : null}
+
+            { showlogoutButton ?
+                <GoogleLogout
+                    clientId={clientId}
+                    buttonText="Sign Out"
+                    onLogoutSuccess={onSignoutSuccess}
+                >
+                </GoogleLogout> : null
+            }
+        </div>
+    );
 
 }
 
