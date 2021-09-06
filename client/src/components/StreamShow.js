c61cee05622e0c6cbfc52ab4a0a4994ad1fb2d30
unknown <tarik.pasalic777@gmail.com>
2021-09-06 17:11:11 +0200

    stream show

--- client/src/components/StreamShow.js
+++ client/src/components/StreamShow.js
@@ -3,6 +3,7 @@ import Form  from "./Form";
 import React,{useEffect,useState} from "react";
 import { useLocation,useHistory } from "react-router-dom";
 import {useSelector} from 'react-redux';
+import flvjs from 'flv.js'
 const StreamShow=()=>{
     const userId=useSelector(state=>state.user.userID);
     const [stream,setStream]=useState([]);
@@ -13,7 +14,23 @@ const StreamShow=()=>{
             axios.get(`http://localhost:3001${location.pathname}`)
             .then(function (response) {
              setStream(response.data);
+            //************************* */
+            if (flvjs.isSupported()) {
+                var videoElement = document.getElementById('videoElement');
+                var flvPlayer = flvjs.createPlayer({
+                    type: 'flv',
+                    url: 'http://localhost:8000/live/stream.flv'
+                });
+                flvPlayer.attachMediaElement(videoElement);
+                flvPlayer.load();
+                flvPlayer.play();
+            }
+
             })
+
+
+
+           
             .catch(function (error) {
               console.log(error);
             });
@@ -25,6 +42,8 @@ const StreamShow=()=>{
     }, [])
  
     return (<div>
+
+      <video id="videoElement"></video>
 
         <h1>   {stream.title} </h1>
         <h3>{stream.description}</h3>
